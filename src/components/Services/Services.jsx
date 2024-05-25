4c0bd052b73c9a47297b1b782b5771018fbf1051
Ayub Onyiego <onyiegoayub@gmail.com>
2024-05-25 15:18:38 +0300

    project initialization

---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,91 @@
+import React, { useContext } from "react";
+import "./Services.css";
+import Card from "../Card/Card";
+import HeartEmoji from "../../img/heartemoji.png";
+import Glasses from "../../img/glasses.png";
+import Humble from "../../img/humble.png";
+import { themeContext } from "../../Context";
+import { motion } from "framer-motion";
+// import Resume from './resume.pdf';
+
+const Services = () => {
+  // context
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+
+  // transition
+  const transition = {
+    duration: 1,
+    type: "spring",
+  };
+
+  return (
+    <div className="services" id="services">
+      {/* left side */}
+      <div className="awesome">
+        {/* dark mode */}
+        <span style={{ color: darkMode ? "white" : "" }}>My Awesome</span>
+        <span>services</span>
+        <spane>
+        I am a versatile software engineer with expertise in full-stack web development.<br /> 
+        I specialize in building robust and scalable web applications that deliver exceptional<br /> 
+        user experiences.<br /> 
+        On the front end, I excel in creating dynamic and responsive interfaces using React.js.<br /> 
+        On the back end, I develop powerful server-side applications with Node.js and Express.js,<br /> 
+        ensuring efficient and smooth operations.
+        </spane>
+        {/* <a href={Resume} download>
+          <button className="button s-button">Download CV</button>
+        </a> */}
+        <div className="blur s-blur1" style={{ background: "#ABF1FF94" }}></div>
+      </div>
+      {/* right */}
+      <div className="cards">
+        {/* first card */}
+        <motion.div
+          initial={{ left: "25rem" }}
+          whileInView={{ left: "14rem" }}
+          transition={transition}
+        >
+          <Card
+            emoji={HeartEmoji}
+            heading={"Web Development"}
+            detail={"Html, Css, JavaScript, React, Nodejs, Express"}
+          />
+        </motion.div>
+        {/* second card */}
+        <motion.div
+          initial={{ left: "-11rem", top: "12rem" }}
+          whileInView={{ left: "-4rem" }}
+          transition={transition}
+        >
+          <Card
+            emoji={Glasses}
+            heading={"App Development"}
+            detail={"React Native, Nodejs, Express"}
+          />
+        </motion.div>
+        {/* 3rd */}
+        <motion.div
+          initial={{ top: "19rem", left: "25rem" }}
+          whileInView={{ left: "12rem" }}
+          transition={transition}
+        >
+          <Card
+            emoji={Humble}
+            heading={"Technical Writing"}
+            detail={
+              "Translate complex technical concepts into clear, concise, and accessible documentation to enhance user understanding and streamline software usage"}
+            color="rgba(252, 166, 31, 0.45)"
+          />
+        </motion.div>
+        <div
+          className="blur s-blur2"
+          style={{ background: "var(--purple)" }}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
